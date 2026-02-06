7542e63fd1d7d9b7804757f08f84c6e7ce9b5685
dsflesd <andyz119@nycstudents.net>
2026-02-05 19:50:15 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
@@ -20,34 +21,73 @@ public class Main {
 
 
         String[] lines = fileData.split("\n");
-
+        int fiveOfAKind = 0;
+        int fourOfAKind = 0;
+        int fullHouse = 0;
+        int threeOfAKind = 0;
+        int doublePair = 0;
+        int singlePair = 0;
+        int highCard = 0;
         for (String line : lines) {
             String[] numbers = line.split(",");
-            String numberSplit= Arrays.toString(numbers);
+            String numberSplit = Arrays.toString(numbers);
+            int barValue = numberSplit.indexOf("|");
+//            int bidValue= Integer.parseInt(numberSplit.substring(barValue+1));
+            numberSplit = numberSplit.substring(1, barValue);
+            String[] cards = numberSplit.split(", ");
 
-
-            int[] values = new int[numbers.length];
-            for (int i=0; i<numbers.length-1; i++) {
-                if (numbers[i].equals("Ace")) {
-                    numbers[i] = "14";
+            for (int i = 0; i < cards.length; i++) {
+                if (cards[i].equals("Ace")) {
+                    cards[i] = "14";
                 }
-                if (numbers[i].equals("King")) {
-                    numbers[i] = "13";
+                if (cards[i].equals("King")) {
+                    cards[i] = "13";
                 }
-                if (numbers[i].equals("Queen")) {
-                    numbers[i] = "12";
+                if (cards[i].equals("Queen")) {
+                    cards[i] = "12";
                 }
-                if (numbers[i].equals("Jack")) {
-                    numbers[i] = "11";
+                if (cards[i].equals("Jack")) {
+                    cards[i] = "11";
                 }
 
             }
-            numberSplit=Arrays.toString(numbers);
-            int barValue = numberSplit.indexOf("|");
-            numberSplit=numberSplit.substring(0, barValue+1);
+            int count = 0;
+            for (String checker : cards) {
+                for (String x:cards) {
+                    if (x.equals(checker)) {
+                        count++;
+                    }
+                }
 
-            System.out.println(numberSplit);
-            int bidValue= Integer.parseInt(numberSplit.substring(barValue));
+            }if (count == 25) {
+                fiveOfAKind++;
+            }
+            if (count == 17) {
+                fourOfAKind++;
+            }
+            if (count == 13) {
+                fullHouse++;
+            }
+            if (count == 11) {
+                threeOfAKind++;
+            }
+            if (count == 9) {
+                doublePair++;
+            }
+            if (count == 7) {
+                singlePair++;
+            }
+            if (count == 5) {
+                highCard++;
+            }
+
         }
+        System.out.println("Five of a kind:" +fiveOfAKind);
+        System.out.println("Four of a kind: "+fourOfAKind);
+        System.out.println("Full House: "+fullHouse);
+        System.out.println("Three of a kind: "+threeOfAKind);
+        System.out.println("Double Pair: "+ doublePair);
+        System.out.println("Single Pair: "+singlePair);
+        System.out.println("High Card: "+highCard);
     }
 }
